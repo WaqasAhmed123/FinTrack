f9e47189c28a48ff9a4294f00d8c07eb856bcd18
Waqas Ahmed <wa02263@gmail.com>
2024-06-11 13:55:38 +0500

    global resources config

--- app/src/main/java/com/example/fintrack/ui/theme/Type.kt
+++ app/src/main/java/com/example/fintrack/ui/theme/Type.kt
@@ -2,32 +2,63 @@ package com.example.fintrack.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.finwise.R
 
 // Set of Material typography styles to start with
+
+val poppinsFontFamily = FontFamily(
+    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_black, FontWeight.Black),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+    Font(R.font.poppins_medium, FontWeight.Medium),
+    Font(R.font.poppins_thin, FontWeight.Thin)
+)
+
 val Typography = Typography(
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )/* Other default text styles to override
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 52.14.sp,
+//        lineHeight = 28.sp,
+//        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+//        lineHeight = 28.sp,
+//        letterSpacing = 0.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = poppinsFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+//        lineHeight = 28.sp,
+//        letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = poppinsFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
+
+
+
 )
+
