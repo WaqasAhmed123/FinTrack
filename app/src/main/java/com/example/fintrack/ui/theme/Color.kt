6d17822796c284f76ed7dcde10600e79c906f964
Waqas Ahmed <wa02263@gmail.com>
2024-06-11 12:41:16 +0500

    initial commit

---
+++ app/src/main/java/com/example/fintrack/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.example.fintrack.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+//light color
+
+val MainGreen = Color(0xFF00D09E)
+val LightGreen = Color(0xFFDFF7E2)
+val Background = Color(0xFFF1FFF3)
+val LightBlueButton = Color(0xFF6DB6FE)
+val BlueButton = Color(0xFF3299FF)
+val OceanBlueButton = Color(0xFF0068FF)
+
+
+//dark mode
+val DarkModeGreenBlack = Color(0xFF031314)
+val BackgroundDarkMode = Color(0xFF052224)
+val LettersandIcons = Color(0xFF093030)
+val DarkModeGreenBar = Color(0xFF0E3E3E)
