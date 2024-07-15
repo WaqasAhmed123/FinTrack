dc4346c2eff711dc1a2e960b5bf58ee1b86ba2c6
Waqas Ahmed <wa02263@gmail.com>
2024-07-15 18:41:51 +0500

    API setup

--- app/src/main/java/com/example/fintrack/data/networkService/NetworkAPIUrls.kt
+++ app/src/main/java/com/example/fintrack/data/networkService/NetworkAPIUrls.kt
@@ -3,7 +3,7 @@ package com.example.fintrack.data.networkService
 object NetworkAPIUrls {
 
 //    const val BASE_URL = "http://localhost:5093"
-    const val BASE_URL = "http://192.168.100.196:5093"
+    const val BASE_URL = "http://192.168.1.103:5093"
     const val STATEMENT_API = "/api/statement"
 
 
